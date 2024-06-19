159a7538af0016ed9cb4f30a0b14f01c221f861d
Robert.Clark <Robert.Clark@terralogic.com>
2024-06-19 12:22:33 -0500

    Implemented and tested local API

--- main.py
+++ main.py
@@ -26,21 +26,22 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = # TODO: enter the path for the saved encoder 
+project_path = ""
+path = encoder_path = os.path.join(project_path, "model", "encoder.pkl")
 encoder = load_model(path)
 
-path = # TODO: enter the path for the saved model 
+path = model_path = os.path.join(project_path, "model", "model.pkl")
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {"message": "hello!"}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -65,10 +66,11 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        data,
+        cat_features,
+        None,
+        False,
+        encoder
     )
-    _inference = # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
